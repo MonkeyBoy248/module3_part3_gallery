1aec3025b0834b12de7640c4f690faecd46016f6
Kirill Temnyi <kirik201100@mail.ru>
2022-05-27 12:15:21 +0300

    refactor: remove unnecessary condition
    MODULE-3-PART-3

--- backend/api/authentication/auth.service.ts
+++ backend/api/authentication/auth.service.ts
@@ -38,9 +38,13 @@ export class AuthService {
   }
 
   logIn = async (userData: RequestUser, dbUserService: DynamoDBUserService, hashService: HashPasswordService, jwtService: JwtService) => {
+    const contender = await dbUserService.getUserByEmail(userData.email);
+
+    if (!contender) {
+      throw new HttpUnauthorizedError('User does not exist');
+    }
+
     try {
-      const contender = await dbUserService.getUserByEmail(userData.email);
-      console.log('contender', contender, contender.email);
       await hashService.comparePasswords(contender?.password.hash, contender.password.salt, userData.password);
 
       return jwtService.createToken(contender.email);
@@ -53,7 +57,7 @@ export class AuthService {
     try {
       return jwtService.verifyToken(token);
     } catch (err) {
-      throw new HttpUnauthorizedError(`${err}`);
+      throw new HttpUnauthorizedError('Invalid token');
     }
   }
 }
